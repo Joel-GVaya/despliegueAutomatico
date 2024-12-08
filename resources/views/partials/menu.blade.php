f382c97d8491be076c392ac8a874e22ecce4cd51
Joel <garciavayajoel@gmail.com>
2024-12-08 18:32:49 +0100

    Commit

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -4,7 +4,7 @@
         <li><a href="/equips" class="text-black hover:underline">Llista d'Equips </a></li><br>
         <li><a href="{{ route('estadis.index') }}" class="text-black hover:underline">Llistat d'Estadis</a></li>
         <li><a href="/jugadors" class="text-black hover:underline">Llistat de Jugadores</a></li>
-        <li><a href="/partits" class="text-black hover:underline">Llistat de Partits</a></li>
+        <li><a href="/partits" class="text-black hover:underline">Llistat de Partits Last test</a></li>
         <li><a href="{{ route('estadis.create') }}" class="text-black hover:underline">Crear Estadi</a></li>
     </ul>
 </nav>
